7b17a33c25bfa46b131cff314502ca3b83375a27
Boris Dayma <boris.dayma@gmail.com>
2019-07-07 09:50:19 -0500

    feat(cnn_kmnist): randomly select hyper parameters

--- benchmarks/cnn_kmnist.py
+++ benchmarks/cnn_kmnist.py
@@ -6,10 +6,26 @@
 # Train a small CNN to identify 10 Japanese characters in classical script
 # Based on MNIST CNN from Keras' examples: https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py (MIT License)
 
+
+# TODO: data augmentation
+# TODO: batch norm
+# TODO: regularization (l2…)
+# TODO: https://keras.io/examples/cifar10_resnet/
+
 from __future__ import print_function
 
 import tensorflow as tf
 from tensorflow.keras import layers
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+
+from pprint import pprint
+
+# For compatibility with RTX 2070
+from tensorflow import keras
+gpu_options = tf.GPUOptions(allow_growth=True)
+sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+keras.backend.set_session(sess)
 
 import argparse
 import numpy as np
@@ -17,19 +33,22 @@ import os
 from utils import load_train_data, load_test_data, load, KmnistCallback
 import wandb
 from wandb.keras import WandbCallback
+import random
+
 
 # default configuration / hyperparameter values
 # you can modify these below or via command line
 MODEL_NAME = ""
 DATA_HOME = "./dataset" 
-BATCH_SIZE = 128
-EPOCHS = 10
-L1_SIZE = 32
-L2_SIZE = 64
-DROPOUT_1_RATE = 0.25
-DROPOUT_2_RATE = 0.5
-FC1_SIZE = 128
+BATCH_SIZE = 2**random.randint(5, 10)  # 8, 16, 32, 64, 128, 256, 512, 1024
+EPOCHS = 100
+FILTERS_INIT = 2**random.randint(4, 6)  # 16, 32, 64
+DROPOUT = random.uniform(0, 1)
+FC_SIZE = 2 ** random.randint(7, 11)  # 128, 256, 512, 1024, 2056
+BLOCKS = random.randint(1, 5)         # resolution too low to have many max pools
+CONV_PER_BLOCK = random.randint(1, 4)
 NUM_CLASSES = 10
+LEARNING_RATE = 10 ** random.uniform(-4, -2)
 #NUM_CLASSES_K49 = 49
 
 # input image dimensions
@@ -45,16 +64,19 @@ def train_cnn(args):
   wandb.init()
   config = {
     "model_type" : "cnn",
-    "batch_size" : args.batch_size,
-    "num_classes" : args.num_classes,
-    "epochs" : args.epochs,
-    "l1_size": args.l1_size,
-    "l2_size" : args.l2_size,
-    "dropout_1" : args.dropout_1,
-    "dropout_2" : args.dropout_2,
-    "fc1_size" : args.fc1_size
+    "batch_size" : BATCH_SIZE,
+    "num_classes" : NUM_CLASSES,
+    "epochs" : EPOCHS,
+    "filters_init": FILTERS_INIT,
+    "dropout": DROPOUT,
+    "blocks": BLOCKS,
+    "conv_per_block": CONV_PER_BLOCK,
+    "fc_size": FC_SIZE,
+    "learning_rate": LEARNING_RATE
   }
   wandb.config.update(config)
+
+  pprint(config)
 
   # Load the data form the relative path provided
   x_train, y_train = load_train_data(args.data_home)
@@ -69,13 +91,13 @@ def train_cnn(args):
   x_test = x_test.astype('float32')
   x_train /= 255
   x_test /= 255
-  if args.quick_run:
-    MINI_TR = 6000
-    MINI_TS = 1000
-    x_train = x_train[:MINI_TR]
-    y_train = y_train[:MINI_TR]
-    x_test = x_test[:MINI_TS]
-    y_test = y_test[:MINI_TS]
+ 
+  # Data augmentation
+  datagen = ImageDataGenerator(
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2)
+  datagen.fit(x_train)
  
   N_TRAIN = len(x_train)
   N_TEST = len(x_test)
@@ -86,29 +108,46 @@ def train_cnn(args):
   y_train = tf.keras.utils.to_categorical(y_train, NUM_CLASSES)
   y_test = tf.keras.utils.to_categorical(y_test, NUM_CLASSES)
 
-  # Build model
+  # Build model (from https://keras.io/examples/cifar10_resnet/)
+
+
+
   model = tf.keras.Sequential()
-  model.add(layers.Conv2D(args.l1_size, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-  model.add(layers.Conv2D(args.l2_size, (3, 3), activation='relu'))
-  model.add(layers.MaxPooling2D(pool_size=(2, 2)))
-  model.add(layers.Dropout(args.dropout_1))
-  model.add(layers.Flatten())
-  model.add(layers.Dense(args.fc1_size, activation='relu'))
-  model.add(layers.Dropout(args.dropout_2))
-  model.add(layers.Dense(args.num_classes, activation='softmax'))
+  filters = FILTERS_INIT
+  model.add(layers.Conv2D(filters, kernel_size=(3, 3),
+            input_shape=input_shape, padding="same"))
+  model.add(layers.BatchNormalization())
+  model.add(layers.Activation('relu'))
+  model.add(layers.Dropout(DROPOUT / 2))
+  for _ in range(CONV_PER_BLOCK - 1):
+    model.add(layers.Conv2D(filters, kernel_size=(3, 3), padding="same"))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Activation('relu'))
+    model.add(layers.Dropout(DROPOUT / 2))
+  for _ in range(BLOCKS - 1):
+    filters *= 2
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    for _ in range(CONV_PER_BLOCK):
+      model.add(layers.Conv2D(filters, kernel_size=(3, 3), padding="same"))
+      model.add(layers.BatchNormalization())
+      model.add(layers.Activation('relu'))
+      model.add(layers.Dropout(DROPOUT / 2))
+  model.add(layers.GlobalAveragePooling2D())
+  model.add(layers.Dropout(DROPOUT))
+  model.add(layers.Dense(NUM_CLASSES, activation='softmax'))
 
   model.compile(loss="categorical_crossentropy",
-              optimizer="adadelta",
+              optimizer=keras.optimizers.Adam(lr=LEARNING_RATE),
               metrics=['accuracy'])
 
-  model.fit(x_train, y_train,
-            batch_size=args.batch_size,
-            epochs=args.epochs,
-            verbose=1,
-            validation_data=(x_test, y_test),
-            callbacks=[KmnistCallback(), WandbCallback(data_type="image", labels=LABELS_10)])
+  model.summary()
+
+  model.fit_generator(datagen.flow(x_train, y_train, batch_size=BATCH_SIZE),
+                      epochs=EPOCHS,
+                      verbose=1,
+                      validation_data=(x_test, y_test),
+                      callbacks=[KmnistCallback(), EarlyStopping(patience=5), ReduceLROnPlateau(),
+                                 WandbCallback(data_type="image", labels=LABELS_10)])
 
   train_score = model.evaluate(x_train, y_train, verbose=0)
   test_score = model.evaluate(x_test, y_test, verbose=0)
@@ -120,76 +159,11 @@ def train_cnn(args):
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
   parser.add_argument(
-    "-m",
-    "--model_name",
-    type=str,
-    default=MODEL_NAME,
-    help="Name of this model/run (model will be saved to this file)")
-  parser.add_argument(
     "--data_home",
     type=str,
     default=DATA_HOME,
     help="Relative path to training/test data")
-  parser.add_argument(
-    "-b",
-    "--batch_size",
-    type=int,
-    default=BATCH_SIZE,
-    help="batch size")
-  parser.add_argument(
-    "--dropout_1",
-    type=float,
-    default=DROPOUT_1_RATE,
-    help="dropout rate for first dropout layer")
-  parser.add_argument(
-    "--dropout_2",
-    type=float,
-    default=DROPOUT_2_RATE,
-    help="dropout rate for second dropout layer")
-  parser.add_argument(
-    "-e",
-    "--epochs",
-    type=int,
-    default=EPOCHS,
-    help="number of training epochs (passes through full training data)")
-  parser.add_argument(
-    "--fc1_size",
-    type=int,
-    default=FC1_SIZE,
-    help="size of fully-connected layer")
-  parser.add_argument(
-    "--l1_size",
-    type=int,
-    default=L1_SIZE,
-    help="size of first conv layer")
-  parser.add_argument(
-    "--l2_size",
-    type=int,
-    default=L2_SIZE,
-    help="size of second conv layer")
-  parser.add_argument(
-    "--num_classes",
-    type=int,
-    default=NUM_CLASSES,
-    help="number of classes (default: 10)")
-  parser.add_argument(
-    "-q",
-    "--dry_run",
-    action="store_true",
-    help="Dry run (do not log to wandb)")
-  parser.add_argument(
-    "--quick_run",
-    action="store_true",
-    help="train quickly on a tenth of the data")   
   args = parser.parse_args()
-
-  # easier testing--don't log to wandb if dry run is set
-  if args.dry_run:
-    os.environ['WANDB_MODE'] = 'dryrun'
-
-  # create run name from command line
-  if args.model_name:
-    os.environ['WANDB_DESCRIPTION'] = args.model_name
 
   train_cnn(args)
 
